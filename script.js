575cb8a0768a73e60926643be59ae5a5904f942d
Liantsoa <liantsoarandrianasimbolarivelo@gmail.com>
2024-05-08 21:19:55 +0300

    8Mai modification

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let rand1
 let rand2
 let rand3
+let score=0;
 document.getElementById("push").onclick=function(){
 function scroll(){
 rand1=Math.floor(Math.random()*4) +1;
@@ -15,10 +16,18 @@ document.getElementById("stop").onclick=function(){
     clearInterval(t);
     console.log(rand1)
     if (rand1==rand2 && rand2==rand3){
-        document.getElementById("result").value="NAHAZO IANAO"
+        document.getElementById("result").value="Nahazo isa 5 ianao"
+        score=score+5
+        document.getElementById("score").value=score
+    }
+    else if (rand1==rand2 || rand2==rand3 || rand1 ==rand3){
+        document.getElementById("result").value="Nahazo isa 2 ianao"   
+        score=score+2
+        document.getElementById("score").value=score
     }
     else{
-        document.getElementById("result").value="TSY NAHAZO IANAO"   
+        document.getElementById("score").value=score
+        document.getElementById("result").value="Tsy nahazo isa ianao"
     }
 }
 }
